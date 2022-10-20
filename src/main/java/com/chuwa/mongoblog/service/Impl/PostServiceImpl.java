448935ffba97fe32dc4ddfb5bd810e209e13406c
Zhiyu Li <rickleezhiyu@gmail.com>
2022-10-19 23:11:10 -0700

    test push v1 after modify

--- src/main/java/com/chuwa/mongoblog/service/Impl/PostServiceImpl.java
+++ src/main/java/com/chuwa/mongoblog/service/Impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import com.chuwa.mongoblog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class PostServiceImpl implements PostService {
 
